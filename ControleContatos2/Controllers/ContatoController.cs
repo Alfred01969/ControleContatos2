fa341010065e98d6a1a8b35671d50c710330ccc7
Alfred01969 <40476087+Alfred01969@users.noreply.github.com>
2022-11-29 16:46:48 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContatos2/Controllers/ContatoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos2.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: Contato
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Apagar()
+        {
+            return View();
+        }
+
+    }
+}
